731b999ae5e8d5b6a9fcd7649973f90279295611
haokr <nothing@nothing.com>
2023-12-27 01:26:58 +0800

    init blog

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "http://localhost", 
+  author: "hk",
+  desc: "你将成为你所想的",
+  title: "鹿上博客",
+  ogImage: "blog-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 10,
+};
+
+export const LOCALE = {
+  lang: "en", 
+  langTag: [],
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  }
+];
---
+++ src/content/blog/demo.md
@@ -0,0 +1,5 @@
+---
+title: demo
+pubDatetime: 2023-11-11 12:00:00
+description: This is demo.
+---
